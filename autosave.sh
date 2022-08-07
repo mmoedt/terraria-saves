983bd843df29ca0893f9e4115e1121944195bf97
Michael Moedt <mmoedt@gmail.com>
2022-08-06 23:38:52 -0700

    auto-snapshot 20220806.2338.52

--- autosave.sh
+++ autosave.sh
@@ -3,6 +3,8 @@ while true; do
   DSTAMP=$(date +%Y%m%d.%H%M.%S)
   echo "Snapshot at '${DSTAMP}'.."
   git add Players Worlds
-  git commit -am "auto-snapshot ${DSTAMP}"
-  sleep 10
+  git commit -am "auto-snapshot ${DSTAMP}" &&
+      echo "## Got it!  Snapshot saved" ||
+          echo "# done"
+  sleep 1
 done
